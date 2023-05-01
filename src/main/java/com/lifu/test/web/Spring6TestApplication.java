31aa9e6ca1bf7511d7bf85bfc9269ce5f9904045
Anthony Ohiero Lifu <tonylifu@outlook.com>
2023-05-01 11:18:00 +0100

    initial setup commit

---
+++ src/main/java/com/lifu/test/web/Spring6TestApplication.java
@@ -0,0 +1,13 @@
+package com.lifu.test.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring6TestApplication.class, args);
+	}
+
+}
